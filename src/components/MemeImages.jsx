a6c5933610138da5169e4d35b51c8c199a0d6667
ginoongflores <florespaul161@gmail.com>
2024-02-19 09:50:19 +0800

    ref(MemeImages): make images responsive on smaller screens
    While also maintaining the aspect ratios

--- src/components/MemeImages.jsx
+++ src/components/MemeImages.jsx
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 const MemeImages = ({ name, url, id }) => {
 	return (
 		<>
-			<section className="container">
-				<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+			<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full w-full">
+				<div className="w-full h-[250px] relative">
 					<Link to={`/meme/${id}`}>
 						<img
-							className="rounded-t-lg object-cover w-full h-64"
+							className="rounded-t-lg absolute top-0 left-0 w-full h-full object-cover"
 							src={url}
 							alt={`${name}`}
 						/>
 					</Link>
-
-					<div className="p-5">
-						<a href="#">
-							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-								{name}
-							</h5>
-						</a>
-					</div>
 				</div>
-			</section>
+
+				<div className="p-5">
+					<a href="#">
+						<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{name}
+						</h5>
+					</a>
+				</div>
+			</div>
 		</>
 	);
 };
